53870acde03965e040a044609b8df5b8cd964fa7
Matthew Carrington-Fair <mcarri01@Matthews-iMac.local>
2018-04-23 21:31:16 -0700

    basic login screen, doesnt pass user data in yet/auth

--- src/realapp/Blockchain-Medical/Blockchain-Medical/AppDelegate.swift
+++ src/realapp/Blockchain-Medical/Blockchain-Medical/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        if Auth.auth().currentUser == nil {
+            let rootController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "login")
+            self.window?.rootViewController = rootController
+        }
         return true
     }
 
